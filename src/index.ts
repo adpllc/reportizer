f9b7a666da5c4a745d443d99af4e2d525e725110
Pete Wookey <pwookey@ad.esi.adp.com>
2020-01-02 17:16:53 +0000

    fix: set json content type

--- src/index.ts
+++ src/index.ts
@@ -77,6 +77,7 @@ export default class ReportPortalClient {
     formData.append(
       'json_request_part',
       JSON.stringify(request),
+      { contentType: 'application/json' },
     );
     if (namedFileBuffer) {
       formData.append(
